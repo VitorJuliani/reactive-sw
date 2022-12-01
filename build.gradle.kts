0c29967c8c25fe24e065351e0b3e607a1a0ad6a2
Vitor Juliani <vitorjuliani99@gmail.com>
2022-12-01 19:12:34 -0300

    add blockhound to check code thread pool

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug")
+	implementation("io.projectreactor.tools:blockhound:1.0.6.RELEASE")
 	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
--- src/main/kotlin/com/example/starwars/StarwarsApplication.kt
+++ src/main/kotlin/com/example/starwars/StarwarsApplication.kt
@@ -1,12 +1,23 @@
 package com.example.starwars
 
+import kotlinx.coroutines.debug.CoroutinesBlockHoundIntegration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import reactor.blockhound.BlockHound
+import reactor.blockhound.integration.ReactorIntegration
+import reactor.blockhound.integration.RxJava2Integration
+import reactor.blockhound.integration.StandardOutputIntegration
 
 @SpringBootApplication
 class StarwarsApplication
 
 fun main(args: Array<String>) {
+    BlockHound
+        .builder()
+        .with(CoroutinesBlockHoundIntegration())
+        .with(StandardOutputIntegration())
+        .with(ReactorIntegration())
+        .with(RxJava2Integration())
+        .install()
     runApplication<StarwarsApplication>(*args)
 }
